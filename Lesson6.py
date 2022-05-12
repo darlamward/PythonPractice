8334c27a2eccce3a5592536a54b21cda858801cb
darlamward <darlamward@gmail.com>
2022-05-12 10:00:53 -0230

    variable name changes

--- Lesson6.py
+++ Lesson6.py
@@ -17,10 +17,10 @@ downPayment = 4789.99
 holidayHST = holidayCost * HST
 totalDue = (holidayCost + holidayHST) - downPayment
 
-holidayCost = "${:,.2f}".format(holidayCost)
-holidayHST = "${:,.2f}".format(holidayHST)
-downPayment = "${:,.2f}".format(downPayment)
-totalDue = "${:,.2f}".format(totalDue)
+holidayCostDsp = "${:,.2f}".format(holidayCost)
+holidayHSTDsp = "${:,.2f}".format(holidayHST)
+downPaymentDsp = "${:,.2f}".format(downPayment)
+totalDueDsp = "${:,.2f}".format(totalDue)
 
 print()
 print("{:<73}".format("  HOLIDAY PLUS - CUSTOMER CONFIRMATION"))
@@ -29,11 +29,12 @@ print("  Location: {:<20s}  Departure Date: {:<8s}   # weeks: {:<1d}".format(loc
 print()
 print("  Customer:", " " * 33, "Charges:")
 print()
-print("  {:<24s}                    Holiday Cost:   {:<9s}".format(customer, holidayCost))
-print("  {:<24s}                    Tax:            {:<9s}".format(street, holidayHST))
-print("  {},{:<2s} {:<6s}                        Down Payment:   {:<9s}".format(city, province, zipCode, downPayment))
+print("  {:<24s}                    Holiday Cost:   {:<9s}".format(customer, holidayCostDsp))
+print("  {:<24s}                    Tax:            {:<9s}".format(street, holidayHSTDsp))
+print("  {},{:<2s} {:<6s}                        Down Payment:   {:<9s}".format(city, province, zipCode,
+                                                                                downPaymentDsp))
 print("{:>71}".format("-" * 25))
-print("{:>71}".format("Total Due:      {:<9s}".format(totalDue)))
+print("{:>71}".format("Total Due:      {:<9s}".format(totalDueDsp)))
 print()
 print("{:<71}".format("  Authorization: ___________________"))
 print()
